f637507ab4208042c64ee903c173c902af83309a
LeSaltyy <isaadk340@gmail.com>
2025-10-12 23:03:20 -0400

    more fix attempts

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name="library-management",
     version="0.1.0",
-    packages=find_packages(),
+    py_modules=['library_service', 'database'],
+    package_dir={'': '.'},
     install_requires=[],
     python_requires='>=3.8',
 )
